6070ab0a3b1a256d3a61a5e2295c283c45865f90
Jorge Calatayud Maeso <jorge.cmaeso@alumnos.upm.es>
2017-11-20 23:48:34 +0100

    Visualizacion
    Mux Bueno

--- MUX4x4.vhd
+++ MUX4x4.vhd
@@ -42,10 +42,10 @@ architecture a_MUX4x4 of MUX4x4 is
 
 begin
 
-Y <= E0 when S="00" else  -- se selecciona la salida en función de las entradas
-     E1 when S="01" else  -- de control
-     E2 when S="10" else
-     E3 when S="11";
+Y <= E0 when S="11" else  -- se selecciona la salida en función de las entradas
+     E1 when S="10" else  -- de control
+     E2 when S="01" else
+     E3 when S="00";
 
 end a_MUX4x4;
 
